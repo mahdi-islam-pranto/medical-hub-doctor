a810b214212795c9ca6589d855defaef79c2e0a7
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2024-09-01 23:10:44 +0600

    Welcome message changed and login middleware removed

--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -3,7 +3,7 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-6 py-3 wow fadeInUp">
-            <h1 style="font-size: 2rem; font-weight: bold;">Welcome to Your Health <br> Center</h1>
+            <h1 style="font-size: 2rem; font-weight: bold;">Welcome to Our Medical <br> Center</h1>
             
             <p class="text-grey mb-4">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Accusantium aperiam earum ipsa eius, inventore nemo labore eaque porro consequatur ex aspernatur. Explicabo, excepturi accusantium! Placeat voluptates esse ut optio facilis!</p>
             <a href="#appointment" class="btn btn-info">Consult With a Doctor</a>
